4d74a835d772081fac29987a10f2bd3931759a68
Natallia Bialevich <natalibelevich94@gmail.com>
2020-03-22 23:41:24 +0300

    feat: solve the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    const regexps = bracketsConfig.map(bracketsArr => bracketsArr.join(''));
+
+    const removeInside = (str) => {
+        let buffer = str;
+        regexps.forEach(regexp => buffer = buffer.replace(regexp, ''));
+        return str === buffer ? buffer : removeInside(buffer);
+    };
+
+    return !removeInside(str).length;
+};
